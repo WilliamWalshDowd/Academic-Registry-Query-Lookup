3f7205f817ecdfee828c9d1e95b3a85ca08a8cc3
William Walsh-Dowd <99445178+WilliamWalshDowd@users.noreply.github.com>
2023-07-26 12:16:00 +0100

    scrapper updates
    scrappy updated to be able to add more than 1 sitemap data to a data file.

--- AR AI/ScrappingCorner/ScrappyMcScrape.py
+++ AR AI/ScrappingCorner/ScrappyMcScrape.py
@@ -37,14 +37,23 @@ def getDataJson(link):
                     details = ""
                 #print("     " + str(n))
                 currentH3 = n.getText()
-            elif ('<p>' in str(n) or '<span>' in str(n) or '<h3>' in str(n))and(idx != 0):
+            elif ('<p>' in str(n) or '<span>' in str(n) or '<h3>' in str(n) or '</a>' in str(n) or '<li>' in str(n))and(idx != 0):
                 #print("     " + n.getText())
                 if (details != "") and (n.getText() != details):
-                    details = details + ", " + n.getText()
+                    if ('</a>' in str(n)) and ('<p>' not in str(n)):
+                        details = details + n.getText() + ", " + str(n.get("href"))
+                    else:
+                        details = details + ", " + n.getText()
                 elif(n.getText() != ""):
-                    details = n.getText()
+                    if ('</a>' in str(n)) and ('<p>' not in str(n)):
+                        details = n.getText() + ', ' + str(n.get("href"))
+                    else:
+                        details = n.getText()
                 #print(details)
         newList.update({escapeString(currentH3):escapeString(details)})
+
+        # add link to list
+        newList.update({'link':link})
 
         finalList = {}
         for key,value in newList.items():
@@ -63,8 +72,8 @@ def saveToJSON(list, path):
     with open(path, "w", encoding="utf-8") as outfile:
         outfile.write(jsonObject)
 
-if __name__ == "__main__":
-    htmldata = getdata('https://www.tcd.ie/academicregistry/sitemap/')
+def makeSitemapJson(sitemap):
+    htmldata = getdata(sitemap)
     soup = BeautifulSoup(htmldata, 'html.parser')
     sitemap  = soup.find('div', class_ = 'section-sitemap')
 
@@ -87,7 +96,13 @@ if __name__ == "__main__":
                     if (str(i).replace(' ', '') != "") and (str(j).replace(' ', '') != ""):
                         #print('i     ' + str(i))
                         #print('j     ' + str(j))
-                        jsonList.append({'Title':name + ' - ' + str(i), 'Content':str(j)})
+                        jsonList.append({'Title':name + ' - ' + str(i), 'Content':str(j), 'Link':str('https:' + link)})
                         #print(jsonList)
+    return jsonList
 
-    saveToJSON(jsonList, '../DataFiles/AcademicRegData.json')
+if __name__ == "__main__":
+    allJsons = {}
+    allJsons.update({'Academic Registry':makeSitemapJson('https://www.tcd.ie/academicregistry/sitemap/')})
+    allJsons.update({'Study':makeSitemapJson('https://www.tcd.ie/study/sitemap/')})
+
+    saveToJSON(allJsons, '../DataFiles/AcademicRegData.json')
